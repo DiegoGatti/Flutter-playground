2f2a02f3dceaaf9b7a74e8daea2d973e538adc22
Diego Gatti <falassionl@gmail.com>
2020-08-13 00:50:08 -0300

    deploy go server and go to http://localhost:8080/build/web/#/

--- hello_world/main.go
+++ hello_world/main.go
@@ -6,15 +6,18 @@ import (
 	"time"
 )
 
+func main() {
+	// http.HandleFunc("/", redirect)
+	// http.ListenAndServe(":8080", nil)
+
+	http.Handle("/build/web/", http.StripPrefix("/build/web/", http.FileServer(http.Dir("build/web"))))
+	http.ListenAndServe(":8080", nil)
+}
+
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World! %s", time.Now())
 }
 
 func redirect(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "index.html", 301)
-}
-
-func main() {
-	http.HandleFunc("/", redirect)
-	http.ListenAndServe(":8080", nil)
 }
